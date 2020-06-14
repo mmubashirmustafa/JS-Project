42791ed12654fa0a7d26384747f6dae9b67e2f2f
mmubashirmustafa <muhammadmubashirmustafa@gmail.com>
2020-06-14 17:19:23 +0500

    JS Project

---
+++ app.js
@@ -0,0 +1,23 @@
+// === Cheapter # 1 and Task # 1:
+
+alert("Welcome To Our Website!!!!")
+
+// === Cheapter # 1 and Task # 2:
+
+alert("Error! Please enter a valid passward.")
+
+// === Cheapter # 1 and Task # 3:
+
+alert("Welcome To JS Land...\n Happy Coading")
+
+// === Cheapter # 1 and Task # 4:
+
+alert("Welcome to JS Land")
+
+// === Cheapter # 1 and Task # 5:
+
+console.log ("Hello... I can run JS through my web browser's console")
+
+// === Cheapter # 2 and Task # 1:
+
+
